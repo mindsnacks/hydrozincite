3223b37adc036dc44e5adbd3afe2c19ec47d6d97
Bryce Culhane <bryce@mindsnacks.com>
2012-08-09 19:04:52 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , Zinc = require('./zinc')
+  , auth = express.basicAuth('mindsnacks', 'nicksdamns');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+var zinc = new Zinc('mindsnacks-zinc.s3.amazonaws.com');
+
+app.get('/', routes.index);
+
+app.get('/:catalog', zinc.ensureCatalog(), function(req, res) {
+  catalog = req.params.catalog;
+  res.format({
+    html: function(){
+      var vars = {
+        title: 'Bundles for ' + catalog,
+        data: zinc.catalogs[catalog]
+      }
+      res.render('catalog', vars);
+    },
+    
+    json: function(){
+      res.json(zinc.catalogs[catalog]);
+    }
+  });
+});
+
+app.get('/:catalog/:bundle', zinc.ensureManifest(), function(req, res) {
+  var catalog = req.params.catalog,
+      bundle = req.params.bundle;
+  res.format({
+    html: function(){
+      var vars = {
+        title: 'Files for ' + bundle,
+        data: zinc.catalogs[catalog].manifests[bundle],
+        bundle: bundle,
+        catalog: req.params.catalog
+      }
+      res.render('bundle', vars);
+    },
+
+    json: function(){
+      res.json(zinc.catalogs[catalog].manifests[req.params.manifest]);
+    }
+  });
+});
+
+app.get('/:catalog/:bundle/*', zinc.ensureManifest(), function(req, res) {
+  var file = req.params[0],
+      type = file.split('.').pop();
+  zinc.getFile(req.params.catalog, req.params.bundle, file, function (data) {
+    res.attachment(file);
+    res.send(data);
+  });
+});
+
+app.all('/settings', auth, function (req, res) {
+  loadSettings(req.query);
+  res.send('OK!');
+});
+
+function loadSettings(settings) {
+  for (key in settings) {
+    app.set('settings ' + key, settings[key]);
+  }
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
